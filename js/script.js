2cd1ab0cf0a26c2f024eb7c09bca109a0aa09967
DanieleBaggiano <151195732+DanieleBaggiano@users.noreply.github.com>
2024-03-26 18:10:04 +0100

    modifiche

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-// chiedo al passeggero quanti chilometri deve percorrere
+// INPUT
 let userKm = prompt("Quanti chilometri devi percorrere?"); // string | null
 console.log(userKm, typeof userKm);
 
-// chiedo al passeggero quanti anni ha
 let userAge = prompt("Quanti anni hai?") // string | null
 console.log(userAge, typeof userAge);
 
@@ -12,22 +11,44 @@ console.log(userKm, typeof userKm);
 userAge = parseInt(userAge); // number
 console.log(userAge, typeof userAge);
 
+// PARTE LOGICA
 const prezzoAlKm = 0.21;
-let total = userKm * prezzoAlKm;
 
+let prezzoBase = userKm * prezzoAlKm; //  number
+console.log(prezzoBase, typeof prezzoBase);
+
+let sconto = 0
+
+if (userAge < 18) {
+    sconto = 20;
+} else if (userAge > 65) {
+    sconto = 40;
+} 
+
+console.log("Sconto in %", sconto);
+const scontoTotale = prezzoBase * sconto / 100;
+console.log("sconto in €", scontoTotale);
+
+const prezzoFinale = prezzoBase - scontoTotale;
+console.log("prezzo finale", prezzoFinale);
+
+// OUTPUT
 if (isNaN(userKm) === false && isNaN(userAge) === false && userKm > 0 && userAge > 0) {
-    if (userAge < 18) {
-        let scontoMinorenne = total * 0.80;
-        console.log("Il passeggero ha diritto al 20% di sconto." + scontoMinorenne);
-        document.getElementById("prezzototale").innerHTML = `${total.toFixed(2)} €, ${scontoMinorenne.toFixed(2)} €`
-    } else if (userAge > 65) {
-        let scontoAnziani = total * 0.60;
-        console.log("Il passeggero ha diritto al 40% di sconto." + scontoAnziani);
-        document.getElementById("prezzototale").innerHTML = `${total.toFixed(2)} €, ${scontoAnziani.toFixed(2)} €`
-    } else {
-        console.log(total.toFixed(2));
-        document.getElementById("prezzototale").innerHTML = `${total.toFixed(2)} €`
-    }
-} else {
-    alert ("Dati errati!");
+    const messaggio = `
+    <dl>
+        <dt>Prezzo base:</dt>
+        <dd>€ ${prezzoBase.toFixed(2)}</dd>
+
+        <dt>Sconto:</dt>
+        <dd>${sconto} %</dd>
+
+        <dt>Prezzo finale:</dt>
+        <dd>${prezzoFinale.toFixed(2)}</dd>
+    </dl>
+    `;    
+
+console.log(messaggio );    
+document.getElementById("prezzototale").innerHTML = messaggio ;
+}    else {
+    alert("Dati errati!");
 }
